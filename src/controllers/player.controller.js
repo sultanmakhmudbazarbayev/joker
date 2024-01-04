0a8083efc12705e313ce37e20d3ff114054efab3
sultanmakhmudbazarbayev <sultanmakhmudbazarbayev@gmail.com>
2024-01-05 02:57:44 +0600

    add player creation

---
+++ src/controllers/player.controller.js
@@ -0,0 +1,35 @@
+import * as Yup from "yup";
+import Player from "../models/Player";
+import { ValidationError } from "~/src/utils/ApiError";
+
+let controller = {
+    create: async (req, res, next) => {
+    try {
+    const values = {
+        name: req.body.name,
+        team_id: req.body.team_id,
+    }
+
+    const schema = Yup.object()
+        .shape({
+            name: Yup.string().required(),
+            team_id: Yup.string().required(),
+
+        });
+
+    if (!(await schema.isValid(values))) {
+        throw new ValidationError();
+    } 
+
+    await Player.create(values)
+
+    return res.status(200).json({
+        msg: "success",
+    });
+    } catch (error) {
+      next(error);
+    }
+  },
+};
+
+export default controller;
---
+++ src/routes/player.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import controller from "../controllers/player.controller";
+import { verifyAdmin } from "../middlewares/admin/auth.middleware.js";
+
+const route = Router();
+route.post("/player", [verifyAdmin], controller.create);
+
+
+export default route;
